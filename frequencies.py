d80994742753f7fcc097c7cff392b3e22a088522
Ronan Mulligan <102162036+RonanMM@users.noreply.github.com>
2022-10-20 18:47:09 +0100

    Add files via upload

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,12 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for item in items:
+        item = str(item)
+        if item in frequencies.keys():
+            count = frequencies.get(item)
+            frequencies.update({item: count +1})
+        else:
+            frequencies[item] = 1
+
     return frequencies
